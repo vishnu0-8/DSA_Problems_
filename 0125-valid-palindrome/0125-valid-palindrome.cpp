50e6f6734505644adf44ddb841387192ce0b566b
vishnu0-8 <prajapativishnug08@gmail.com>
2026-08-14 09:47:59 +0530

    Time: 0 ms (100%), Space: 10 MB (54.01%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int start=0;
+        int end=s.length()-1;
+        while(start<end){
+            if(!isalnum(s[start])){
+                start++;
+            }
+            else if(!isalnum(s[end])){
+                end--;
+            }
+            else{
+                if(tolower(s[start])!=tolower(s[end])){
+                    return false;
+                }
+                else{
+                    start++;
+                    end--;
+                }
+            }
+            
+        }
+        return true;
+    }
+};
